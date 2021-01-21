0f9ac2c3c68a1acd5155e41c5a90e456f562cea2
Douglas Horn <37677685+DouglasHorn@users.noreply.github.com>
2021-01-20 23:49:21 -0800

    Revise unfreeze epochs to 120 (5 days @ 24/dy)

--- protocol/contracts/mock/MockState.sol
+++ protocol/contracts/mock/MockState.sol
@@ -83,7 +83,7 @@ contract MockState is Setters {
     }
 
     function unfreezeE(address account) external {
-        super.unfreeze(account);
+        super.unfreeze(account, 120); // Hardcode 120 epochs to unfreeze - 5 days @ 24 epochs per day
     }
 
     function updateAllowanceCouponsE(address owner, address spender, uint256 amount) external {
