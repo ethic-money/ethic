c308bb7859a72d2331c707a798342a5a609dd926
Douglas Horn <37677685+DouglasHorn@users.noreply.github.com>
2021-02-01 16:10:29 -0800

    Update truffle.js
    Add Kovan testnet

--- protocol/truffle.js
+++ protocol/truffle.js
@@ -76,6 +76,15 @@ module.exports = {
       skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
     },
 
+    kovan: {
+      provider: () => new PrivateKeyProvider(privateKey, 'https://kovan.infura.io/v3/' + infuraId),
+      network_id: 42,       // Kovan's id
+      gas: 5000000,        // Kovan has a lower block limit than mainnet
+      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    },
+
     // Useful for private networks
     // private: {
       // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
