10646860549d3ca4b3ef3925ed5aec9c0d7c0d46
Douglas Horn <37677685+DouglasHorn@users.noreply.github.com>
2021-02-01 16:02:29 -0800

    Create 3_deploy_token.js

---
+++ protocol/migrations/3_deploy_token.js
@@ -0,0 +1,25 @@
+@@ -0,0 +1,26 @@
+/** const Implementation = artifacts.require("Ethic");
+async function deployImplementation(deployer) {
+  await deployer.deploy(Implementation);
+}
+module.exports = function(deployer) {
+  deployer.then(async() => {
+    console.log(deployer.network);
+    switch (deployer.network) {
+      case 'development':
+      case 'rinkeby':
+      case 'kovan':
+      case 'ropsten':
+        await deployImplementation(deployer);
+        break;
+      case 'mainnet':
+      case 'mainnet-fork':
+        await deployImplementation(deployer);
+        break;
+      default:
+        throw("Unsupported network");
+    }
+  })
+};
+**/
